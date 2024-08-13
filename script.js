d65ffafbee1a30f9425bc64601f0f0b1080a337d
BiggStepper <kwame.bixcuit@gmail.com>
2024-08-13 14:26:46 +0000

    7th commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 const menuBars = document.getElementById('menu-bars');
 const crossBars = document.getElementById('menu-xmarks');
+const navMenu = document.getElementById('menu-nav');
 
 menuBars.addEventListener('click', () => {
     menuBars.style.display = 'none';
     crossBars.style.display = 'block';
+    navMenu.style.display = 'block';
 });
 
 crossBars.addEventListener('click', () => {
     crossBars.style.display = 'none';
     menuBars.style.display = 'block';
+    navMenu.style.display = 'none';
 });
