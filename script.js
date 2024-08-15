76db72bfeba482ea47f115eef26de6ed65c1f554
BiggStepper <kwame.bixcuit@gmail.com>
2024-08-15 12:33:32 +0000

    added animations to the cards

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const menuBars = document.getElementById('menu-bars');
 const crossBars = document.getElementById('menu-xmarks');
 const navMenu = document.getElementById('menu-nav');
+const main = document.getElementById('main');
 
 menuBars.addEventListener('click', () => {
     menuBars.style.display = 'none';
@@ -10,6 +11,13 @@ menuBars.addEventListener('click', () => {
 });
 
 crossBars.addEventListener('click', () => {
+    crossBars.style.display = 'none';
+    menuBars.style.display = 'block';
+    navMenu.style.display = 'none';
+    document.getElementsByClassName('cssanimation fadeOutRight')
+});
+
+main.addEventListener('click', () => {
     crossBars.style.display = 'none';
     menuBars.style.display = 'block';
     navMenu.style.display = 'none';
